9accc484cb9ca2c4e49b622c7d7bdc721946d9f0
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2025-02-14 09:11:26 +0100

    elprimero

---
+++ src/main/java/com/example/demo/modelos/Futbolista.java
@@ -0,0 +1,109 @@
+package com.example.demo.modelos;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the futbolista database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Futbolista.findAll", query="SELECT f FROM Futbolista f")
+public class Futbolista implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+
+	public Futbolista(int edad, Date fechaNac, int idUsuario, String nacionalidad, String nombre, Club club) {
+		super();
+		this.edad = edad;
+		this.fechaNac = fechaNac;
+		this.idUsuario = idUsuario;
+		this.nacionalidad = nacionalidad;
+		this.nombre = nombre;
+		this.club = club;
+	}
+
+	@Id
+	private int id;
+
+	private int edad;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="fecha_nac")
+	private Date fechaNac;
+
+	@Column(name="id_usuario")
+	private int idUsuario;
+
+	private String nacionalidad;
+
+	private String nombre;
+
+	//bi-directional many-to-one association to Club
+	@ManyToOne
+	@JoinColumn(name="id_club")
+	private Club club;
+
+	public Futbolista() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Date getFechaNac() {
+		return this.fechaNac;
+	}
+
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public int getIdUsuario() {
+		return this.idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNacionalidad() {
+		return this.nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Club getClub() {
+		return this.club;
+	}
+
+	public void setClub(Club club) {
+		this.club = club;
+	}
+
+}
---
+++ src/main/java/com/example/demo/modelos/Usuario.java
@@ -0,0 +1,131 @@
+package com.example.demo.modelos;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+
+/**
+ * The persistent class for the usuario database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public Usuario(byte admin, String aficiones, String apellidos, String email, String nombre, String pais,
+			String password, String sexo) {
+		super();
+		this.admin = admin;
+		this.aficiones = aficiones;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.nombre = nombre;
+		this.pais = pais;
+		this.password = password;
+		this.sexo = sexo;
+	}
+
+	private byte admin;
+
+	private String aficiones;
+
+	private String apellidos;
+
+	private String email;
+
+	@Id
+	private int id;
+
+	private String nombre;
+
+	private String pais;
+
+	private String password;
+
+	private String sexo;
+
+	
+
+	
+
+	public Usuario() {
+		super();
+	}
+
+	
+
+
+	public byte getAdmin() {
+		return this.admin;
+	}
+
+	public void setAdmin(byte admin) {
+		this.admin = admin;
+	}
+
+	public String getAficiones() {
+		return this.aficiones;
+	}
+
+	public void setAficiones(String aficiones) {
+		this.aficiones = aficiones;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPais() {
+		return this.pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSexo() {
+		return this.sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+}
